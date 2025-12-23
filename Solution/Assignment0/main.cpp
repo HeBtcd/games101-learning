e56cb6d9a41a5fb6312d6a75d70b5b5bf0d10b42
HeBtcd <1115692643@qq.com>
2025-12-23 15:51:20 +0800

    Assignment0 refactored

--- Solution/Assignment0/main.cpp
+++ Solution/Assignment0/main.cpp
@@ -2,20 +2,27 @@
 #include <iostream>
 #include <cmath>
 
+const float PI = std::acos(-1.0f); // cos(pointi) = -1, arccos(-1) = pointi.
+
 Eigen::Vector3f solve(const Eigen::Vector3f& vector){
     // 绕原点先逆时针旋转 45◦, 再平移 (1,2).
-    float pointi = std::acos(-1.0f); // cos(pointi) = -1, arccos(-1) = pointi.
-    float angle = 45.0f / 180.0f * pointi; // 弧度制 -> 角度制.
-    float cos45 = std::cos(angle);
-    float sin45 = std::sin(angle);
+    float rad = 45.0f / 180.0f * PI; // 角度制 -> 弧度制.
+    float cos45 = std::cos(rad);
+    float sin45 = std::sin(rad);
     
-    Eigen::Matrix3f transform;
+    Eigen::Matrix3f rotation;
+    rotation << cos45, -sin45, 0., 
+                sin45,  cos45, 0., 
+                   0.,     0., 1.;
+    
     float dx = 1.0f;
     float dy = 2.0f;
-    transform << cos45, -sin45,   dx, 
-             sin45,  cos45,   dy, 
-              0.0f,   0.0f, 1.0f;
-    return transform * vector;
+    Eigen::Matrix3f translation;
+    translation << 1.0f, 0.0f, dx,
+                   0.0f, 1.0f, dy,
+                   0.0f, 0.0f, 1.0f;
+    
+    return translation * rotation * vector;
 }
 
 int main(){
